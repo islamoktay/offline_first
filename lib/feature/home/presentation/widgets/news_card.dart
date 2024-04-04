033fe219f24d84e2cd0d1e5999bee804fb156f10
Ahmet Islam Oktay <43931758+islamoktay@users.noreply.github.com>
2024-04-04 10:27:34 +0300

    added cache mechanism to network images

--- lib/feature/home/presentation/widgets/news_card.dart
+++ lib/feature/home/presentation/widgets/news_card.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 
 import 'package:offline_first/core/dependency_injection/di.dart';
+import 'package:offline_first/core/extension/num_x.dart';
+import 'package:offline_first/core/services/cache_service/local_cache_manager.dart';
 import 'package:offline_first/core/services/navigation_service/i_navigation_service.dart';
 import 'package:offline_first/feature/home/domain/entity/article.dart';
 import 'package:offline_first/feature/news_detail/presentation/view/news_detail_view.dart';
@@ -36,7 +38,11 @@ class NewsCard extends StatelessWidget {
             ),
             const SizedBox(height: 8),
             Text('by ${article.authorVal ?? ''}'),
-            CachedNetworkImage(imageUrl: article.urlToImageVal ?? ''),
+            CachedNetworkImage(
+              imageUrl: article.urlToImageVal ?? '',
+              memCacheHeight: 200.cacheSize(context),
+              cacheManager: LocalCacheManager.instance,
+            ),
           ],
         ),
       ),
--- lib/feature/splash/presentation/view/splash_view.dart
+++ lib/feature/splash/presentation/view/splash_view.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter_bloc/flutter_bloc.dart';
-
 import 'package:offline_first/core/constant/gen/assets.gen.dart';
 import 'package:offline_first/core/dependency_injection/di.dart';
 import 'package:offline_first/core/services/navigation_service/i_navigation_service.dart';
-import 'package:offline_first/feature/home/presentation/provider/cubit/news_cubit.dart';
 import 'package:offline_first/feature/home/presentation/view/home_view.dart';
 
 class SplashView extends StatelessWidget {
